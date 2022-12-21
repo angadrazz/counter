cf1a9c9146328bff6ff21efd88622aa40d13bf52
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-21 10:50:17 +0530

    setinterval

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 var current = document.querySelector('.current');
-var next = document.querySelector('.next')
+var next = document.querySelector('.next');
+
+function startCounter(){
+    var interval = setInterval(animate, 1000);
+        
+}
